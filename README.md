35163a4b63e309e09c992d857fa2138c4cafa73d
Jonathan Marler <johnnymarler@gmail.com>
2022-06-19 19:33:13 -0600

    initial implementation

---
+++ README.md
@@ -0,0 +1,3 @@
+# static-python3
+
+This repo builds and hosts a static python3 interpreter for linux x86_64.
---
+++ common.py
@@ -0,0 +1,6 @@
+import os
+
+#PYTHON_VERSION = "3.10.5"
+PYTHON_VERSION = "3.9.13"
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+OUT_DIR = os.path.join(SCRIPT_DIR, "out")
